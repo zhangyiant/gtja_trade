15d3da51322405706fca0476a35f79a877778f1e
Yi Zhang <zhangyiant@hotmail.com>
2016-01-17 20:23:16 +0800

    Add Trade class.

---
+++ icbc/Trade.py
@@ -0,0 +1,25 @@
+'''
+Created on 2016年1月17日
+
+@author: Wenwen
+'''
+
+class Trade:
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pass
+    
+    
+    def login(self):
+        pass
+
+if __name__ == '__main__':
+    pass
+    
