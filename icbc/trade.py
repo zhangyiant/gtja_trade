30eafac0a07aab194a78f2072a80c05276c6e2f4
Yi Zhang <zhangyiant@hotmail.com>
2016-01-18 20:45:47 +0800

    Add logging service.

--- icbc/trade.py
+++ icbc/trade.py
@@ -3,6 +3,7 @@ Created on 2016年1月17日
 
 @author: Wenwen
 '''
+import logging
 
 class Trade:
     '''
@@ -15,11 +16,13 @@ class Trade:
         Constructor
         '''
         print("constructor")
+        self.logger = logging.getLogger(__name__)
         return
     
     
     def login(self):
         print("login")
+        self.logger.info("login")
         return
 
 if __name__ == '__main__':
--- icbc_main.py
+++ icbc_main.py
@@ -4,6 +4,25 @@ Created on 2016年1月18日
 @author: Wenwen
 '''
 from icbc.trade import Trade
+import logging
+import configparser
+
+# read configuration
+config = configparser.ConfigParser()
+config.read("gtja_trade.ini", encoding="utf-8")
+account_name = config['Account'].get('account_name')
+password = config['Account'].get('password')
+connection_string = config['Database'].get('connection')
+logging_filename = "icbc.log"
+
+# logger setup
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler(logging_filename, encoding="utf-8")
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
 
 if __name__ == '__main__':
     trade = Trade()
