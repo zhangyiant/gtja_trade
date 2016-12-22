5c2c6b4d37cde0cdc66f061a2653ecc3c15a6d7e
Yi Zhang <zhangyiant@hotmail.com>
2016-12-23 06:25:48 +0800

    Add complete_buy_transaction script.

---
+++ complete_buy_transaction.py
@@ -0,0 +1,39 @@
+import configparser
+import logging
+import sys
+from gtja.stockprocessor import StockProcessor
+from gtja.utility import complete_buy_transaction;
+    
+import stock_db
+
+
+STOCK_SYMBOL = sys.argv[1]
+BUY_PRICE = float(sys.argv[2])
+QUANTITY = int(sys.argv[3])
+
+# read configuration
+CONFIG_PARSER = configparser.ConfigParser()
+CONFIG_PARSER.read("gtja_trade.ini", encoding="utf-8")
+ACCOUNT_NAME = CONFIG_PARSER['Account'].get('account_name')
+PASSWORD = CONFIG_PARSER['Account'].get('password')
+CONNECTION_STRING = CONFIG_PARSER['Database'].get('connection')
+LOGGING_FILENAME = CONFIG_PARSER['Logging'].get('filename')
+
+# logger setup
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.DEBUG)
+logging.getLogger('sqlalchemy.engine').setLevel(logging.WARNING)
+logging.getLogger('selenium.webdriver.remote.remote_connection').setLevel(logging.WARNING)
+FH = logging.FileHandler(LOGGING_FILENAME, encoding="utf-8")
+FORMATTER = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+FH.setFormatter(FORMATTER)
+LOGGER.addHandler(FH)
+
+# set the DB connection string
+stock_db.db_connection.default_connection_string = CONNECTION_STRING
+
+print(STOCK_SYMBOL)
+print(BUY_PRICE)
+print(QUANTITY)
+complete_buy_transaction(STOCK_SYMBOL, BUY_PRICE, QUANTITY)
+
