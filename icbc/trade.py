1b9d26ae47a189efed6b08b1be0f353af78bb0f6
Yi Zhang <zhangyiant@hotmail.com>
2017-01-16 21:47:25 +0800

    Update quanbu element xpath.

--- icbc/trade.py
+++ icbc/trade.py
@@ -66,7 +66,7 @@ class Trade:
     def main_page(self):
         self.logger.debug("main_page")
         self.driver.switch_to_default_content()
-        logo_xpath = "/html/body/boby/div[4]/div/div/img"
+        logo_xpath = "/html/body/boby/div[3]/div/div/img"
         logo_element = WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.XPATH, logo_xpath)))
         logo_element.click()
