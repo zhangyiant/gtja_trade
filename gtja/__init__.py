3e081a06ac9b10849164f8abff2f32aab33aeaf0
Yi Zhang <zhangyiant@hotmail.com>
2017-04-24 20:53:31 +0800

    import class in Trade.py in __init__.py

---
+++ gtja/__init__.py
@@ -0,0 +1,12 @@
+from .Trade import (
+    CurrentCommissionInfo,
+    StockInfo,
+    AccountInfo,
+    Trade)
+
+__all__ = [
+    "CurrentCommissionInfo",
+    "StockInfo",
+    "AccountInfo",
+    "Trade"
+]
