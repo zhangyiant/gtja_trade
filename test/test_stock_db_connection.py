1793f11884eacb7f23632d572e75eacd641040f8
Yi Zhang <zhangyiant@hotmail.com>
2017-05-08 20:56:39 +0800

    Use gtja_trade.ini for test file.

--- test/test_stock_db_connection.py
+++ test/test_stock_db_connection.py
@@ -18,7 +18,7 @@ from datetime import date
 class StockDbConnectionTest(unittest.TestCase):
     def setUp(self):
         config = configparser.ConfigParser()
-        config.read("stock.ini", encoding="utf-8")
+        config.read("gtja_trade.ini", encoding="utf-8")
         connection_string = config['Database'].get('test_connection')
         stock_db.db_connection.default_connection_string = connection_string
         return
