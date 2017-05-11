3618d6c8cbb98a48838adcc9e7656c2aed0f0b61
Yi Zhang <zhangyiant@hotmail.com>
2017-05-11 20:24:40 +0800

    Fixed format issue.

--- test/test_stock_table.py
+++ test/test_stock_table.py
@@ -12,6 +12,7 @@ from stock_db.db_stock import StockClosedTransaction, \
 from stock_db.db_utility import reset_table
 from stock_db.db_connection import get_default_db_connection
 
+
 class StockClosedTransactionTableTest(unittest.TestCase):
     '''
         StockClosedTransactionTableTest class
@@ -43,7 +44,7 @@ class StockClosedTransactionTableTest(unittest.TestCase):
 
         # query and compare
         stock_closed_transaction_list = \
-                transaction_table.get_all_stock_closed_transaction()
+            transaction_table.get_all_stock_closed_transaction()
         self.assertEqual(len(stock_closed_transaction_list),
                          1,
                          "There should be only 1 item")
@@ -65,7 +66,7 @@ class StockClosedTransactionTableTest(unittest.TestCase):
         transaction_table.delete_stock_closed_transaction(
             stock_closed_transaction)
         stock_closed_transaction_list = \
-                transaction_table.get_all_stock_closed_transaction()
+            transaction_table.get_all_stock_closed_transaction()
         self.assertEqual(len(stock_closed_transaction_list),
                          0,
                          "The list should be an empty list")
@@ -79,7 +80,6 @@ class StockClosedTransactionTableTest(unittest.TestCase):
         stock_db_connection = get_default_db_connection()
         reset_table(stock_db_connection)
         stock_transaction_table = StockTransactionTable(stock_db_connection)
-
 
         # init transaction 1
         stock_transaction_1 = StockTransaction()
@@ -147,7 +147,7 @@ class StockClosedTransactionTableTest(unittest.TestCase):
         stock_transaction_1.quantity = 200
         stock_transaction_1.price = 4.51
         stock_transaction_table.add_stock_transaction(stock_transaction_1)
-        trans_id_1 = stock_transaction_1.trans_id
+        stock_transaction_1.trans_id
 
         # init transaction 2
         stock_transaction_2 = StockTransaction()
@@ -157,7 +157,7 @@ class StockClosedTransactionTableTest(unittest.TestCase):
         stock_transaction_2.quantity = 100
         stock_transaction_2.price = 4.81
         stock_transaction_table.add_stock_transaction(stock_transaction_2)
-        trans_id_2 = stock_transaction_2.trans_id
+        stock_transaction_2.trans_id
 
         owned_quantity = StockTransaction.get_owned_quantity("601398")
         self.assertEqual(owned_quantity,
@@ -181,7 +181,7 @@ class StockClosedTransactionTableTest(unittest.TestCase):
         stock_transaction_1.quantity = 200
         stock_transaction_1.price = 4.9
         stock_transaction_table.add_stock_transaction(stock_transaction_1)
-        trans_id_1 = stock_transaction_1.trans_id
+        stock_transaction_1.trans_id
 
         # init transaction 2
         stock_transaction_2 = StockTransaction()
@@ -191,7 +191,7 @@ class StockClosedTransactionTableTest(unittest.TestCase):
         stock_transaction_2.quantity = 100
         stock_transaction_2.price = 4.81
         stock_transaction_table.add_stock_transaction(stock_transaction_2)
-        trans_id_2 = stock_transaction_2.trans_id
+        stock_transaction_2.trans_id
 
         lowest_price = StockTransaction.get_lowest_buy_price("601398")
         self.assertEqual(lowest_price, 4.81)
@@ -204,7 +204,7 @@ class StockClosedTransactionTableTest(unittest.TestCase):
         '''
         stock_db_connection = get_default_db_connection()
         reset_table(stock_db_connection)
-        stock_transaction_table = StockTransactionTable(stock_db_connection)
+        StockTransactionTable(stock_db_connection)
 
         lowest_price = StockTransaction.get_lowest_buy_price("601398")
         self.assertEqual(lowest_price, 9999.00)
@@ -227,7 +227,7 @@ class StockClosedTransactionTableTest(unittest.TestCase):
         stock_transaction_1.quantity = 200
         stock_transaction_1.price = 4.51
         stock_transaction_table.add_stock_transaction(stock_transaction_1)
-        trans_id_1 = stock_transaction_1.trans_id
+        stock_transaction_1.trans_id
 
         # init transaction 2
         stock_transaction_2 = StockTransaction()
@@ -237,10 +237,10 @@ class StockClosedTransactionTableTest(unittest.TestCase):
         stock_transaction_2.quantity = 100
         stock_transaction_2.price = 4.81
         stock_transaction_table.add_stock_transaction(stock_transaction_2)
-        trans_id_2 = stock_transaction_2.trans_id
+        stock_transaction_2.trans_id
 
         with self.assertRaises(Exception):
-            lowest_price = StockTransaction.get_lowest_buy_price("601398")
+            StockTransaction.get_lowest_buy_price("601398")
 
         return
 
@@ -260,7 +260,7 @@ class StockClosedTransactionTableTest(unittest.TestCase):
         stock_transaction_1.quantity = 200
         stock_transaction_1.price = 4.9
         stock_transaction_table.add_stock_transaction(stock_transaction_1)
-        trans_id_1 = stock_transaction_1.trans_id
+        stock_transaction_1.trans_id
 
         # init transaction 2
         stock_transaction_2 = StockTransaction()
@@ -270,7 +270,7 @@ class StockClosedTransactionTableTest(unittest.TestCase):
         stock_transaction_2.quantity = 100
         stock_transaction_2.price = 4.81
         stock_transaction_table.add_stock_transaction(stock_transaction_2)
-        trans_id_2 = stock_transaction_2.trans_id
+        stock_transaction_2.trans_id
 
         quantity = StockTransaction.get_lowest_buy_price_quantity("601398")
         self.assertEqual(quantity, 100)
@@ -283,7 +283,7 @@ class StockClosedTransactionTableTest(unittest.TestCase):
         '''
         stock_db_connection = get_default_db_connection()
         reset_table(stock_db_connection)
-        stock_transaction_table = StockTransactionTable(stock_db_connection)
+        StockTransactionTable(stock_db_connection)
 
         quantity = StockTransaction.get_lowest_buy_price_quantity("601398")
         self.assertEqual(quantity, 0)
@@ -306,7 +306,7 @@ class StockClosedTransactionTableTest(unittest.TestCase):
         stock_transaction_1.quantity = 200
         stock_transaction_1.price = 4.51
         stock_transaction_table.add_stock_transaction(stock_transaction_1)
-        trans_id_1 = stock_transaction_1.trans_id
+        stock_transaction_1.trans_id
 
         # init transaction 2
         stock_transaction_2 = StockTransaction()
@@ -316,9 +316,9 @@ class StockClosedTransactionTableTest(unittest.TestCase):
         stock_transaction_2.quantity = 100
         stock_transaction_2.price = 4.81
         stock_transaction_table.add_stock_transaction(stock_transaction_2)
-        trans_id_2 = stock_transaction_2.trans_id
+        stock_transaction_2.trans_id
 
         with self.assertRaises(Exception):
-            quantity = StockTransaction.get_lowest_buy_price_quantity("601398")
+            StockTransaction.get_lowest_buy_price_quantity("601398")
 
         return
