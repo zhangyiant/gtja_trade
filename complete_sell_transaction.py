973f483d91d1d3986adc2534eaa8035910afb362
Yi Zhang <zhangyiant@hotmail.com>
2017-06-23 22:21:57 +0800

    Remove usued logging.

--- complete_sell_transaction.py
+++ complete_sell_transaction.py
@@ -1,9 +1,13 @@
 import configparser
 import logging
+import logging.config
+from gtja.settings import LOGGING
+logging.config.dictConfig(LOGGING)
+
 import sys
 from gtja.stockprocessor import StockProcessor
 from gtja.utility import complete_sell_transaction;
-    
+
 import stock_db
 
 STOCK_SYMBOL = sys.argv[1]
@@ -16,17 +20,6 @@ CONFIG_PARSER.read("gtja_trade.ini", encoding="utf-8")
 ACCOUNT_NAME = CONFIG_PARSER['Account'].get('account_name')
 PASSWORD = CONFIG_PARSER['Account'].get('password')
 CONNECTION_STRING = CONFIG_PARSER['Database'].get('connection')
-LOGGING_FILENAME = CONFIG_PARSER['Logging'].get('filename')
-
-# logger setup
-LOGGER = logging.getLogger()
-LOGGER.setLevel(logging.DEBUG)
-logging.getLogger('sqlalchemy.engine').setLevel(logging.WARNING)
-logging.getLogger('selenium.webdriver.remote.remote_connection').setLevel(logging.WARNING)
-FH = logging.FileHandler(LOGGING_FILENAME, encoding="utf-8")
-FORMATTER = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-FH.setFormatter(FORMATTER)
-LOGGER.addHandler(FH)
 
 # set the DB connection string
 stock_db.db_connection.default_connection_string = CONNECTION_STRING
@@ -35,4 +28,3 @@ print(STOCK_SYMBOL)
 print(SELL_PRICE)
 print(QUANTITY)
 complete_sell_transaction(STOCK_SYMBOL, SELL_PRICE, QUANTITY)
-
