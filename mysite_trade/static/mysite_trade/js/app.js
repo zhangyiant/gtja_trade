f66087b55a99f71998447e9a961ff2cb0ece6813
Yi Zhang <zhangyiant@hotmail.com>
2017-06-24 21:34:47 +0800

    Add test page

---
+++ mysite_trade/static/mysite_trade/js/app.js
@@ -0,0 +1,28 @@
+(function(angular) {
+  'use strict';
+  var myApp = angular.module('myApp', []);
+
+  myApp.controller(
+    "TradeController",
+    [
+      "$scope",
+      "$http",
+      function($scope, $http) {
+        $scope.post = function() {
+          var data={
+            param1: "hello",
+            param2: "next"
+          };
+          $http.post("/mysite_trade/buy/", data).then(
+            function successCallback(response) {
+              console.log(response);
+            },
+            function errorCallback(response) {
+              console.log(response);
+            }
+          );
+        };
+      }
+    ]
+  );
+})(window.angular);
--- mysite_trade/views.py
+++ mysite_trade/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import (
+    HttpResponse,
+    JsonResponse)
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
 def index(request):
     return HttpResponse("HelloWorld")
+
+@csrf_exempt
+def buy(request):
+    data = json.loads(request.body.decode("utf-8"))
+    print(data)
+    return JsonResponse(data)
