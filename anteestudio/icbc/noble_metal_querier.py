0f166ebb4933cd2f34a30f721bfc030f2e1999f6
antee <zhangyiant@hotmail.com>
2017-06-24 16:52:58 +0800

    Record noble price list.

--- anteestudio/icbc/noble_metal_querier.py
+++ anteestudio/icbc/noble_metal_querier.py
@@ -5,6 +5,34 @@ import AdvancedHTMLParser
 
 LOGGER = logging.getLogger(__name__)
 
+class NobleMetalPrice:
+
+    def __init__(self,
+                 symbol=None,
+                 buy_price=None,
+                 sell_price=None,
+                 middle_price=None,
+                 highest_middle_price=None,
+                 lowest_middle_price=None):
+        self.symbol = symbol
+        self.buy_price = buy_price
+        self.sell_price = sell_price
+        self.middle_price = middle_price
+        self.highest_middle_price = highest_middle_price
+        self.lowest_middle_price = lowest_middle_price
+        return
+        
+    def __str__(self):
+        result = "symbol: {0}\n".format(self.symbol)
+        result += "buy price: {0}\n".format(self.buy_price)
+        result += "sell price: {0}\n".format(self.sell_price)
+        result += "middle price: {0}\n".format(self.middle_price)
+        result += "highest middle price: {0}\n".format(
+            self.highest_middle_price)
+        result += "lowest middle price: {0}\n".format(
+            self.lowest_middle_price)
+        return result
+
 
 class NobleMetalQuerier:
 
@@ -17,6 +45,7 @@ class NobleMetalQuerier:
         parser = AdvancedHTMLParser.AdvancedHTMLParser()
         LOGGER.debug("Get noble metal price list")
         req = urllib.request.Request(url=self.url)
+        price_list = []
         with urllib.request.urlopen(req) as f:
             data = f.read().decode("utf-8")
             parser.parseStr(data)
@@ -31,12 +60,13 @@ class NobleMetalQuerier:
                 middle_price = float(tds[4].innerHTML)
                 highest_middle_price = float(tds[5].innerHTML)
                 lowest_middle_price = float(tds[6].innerHTML)
-                print("{0}:{1}:{2}:{3}:{4}:{5}".format(
-                    symbol,
-                    buy_price,
-                    sell_price,
-                    middle_price,
-                    highest_middle_price,
-                    lowest_middle_price))
-        return
+                price = NobleMetalPrice(
+                    symbol=symbol,
+                    buy_price=buy_price,
+                    sell_price=sell_price,
+                    middle_price=middle_price,
+                    highest_middle_price=highest_middle_price,
+                    lowest_middle_price=lowest_middle_price)
+                price_list.append(price)
+        return price_list
     
